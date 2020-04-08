3b187a714190a2c93b8787f119c4391f5ea96eba
shamshad <shamshad@shamshads-MacBook-Pro.local>
2020-04-08 22:28:37 +0400

    warehouse log report

--- app/Http/Controllers/Admin/WarehouseController.php
+++ app/Http/Controllers/Admin/WarehouseController.php
@@ -9,13 +9,14 @@ use Carbon\Carbon;
 
 use App\Model\Warehouse;
 use App\Model\WarehouseStock;
+use App\Model\LogReport;
 
 class WarehouseController extends Controller
 {
     public function index(Request $request){
         $data_bundle = [];
         $data_bundle['items'] = Warehouse::paginate(20);
-        // dd($data_bundle['buildings']);
+        // dd($data_bundle['items']);
         return view('admin.warehouse.index', compact($data_bundle));
     }
 
@@ -42,17 +43,29 @@ class WarehouseController extends Controller
 		} else {
             // $validated = $request->validated();
             if($id){
-                $building = Warehouse::find($id);
-                $building->item_name = $request->input('item_name');
-                $user->save();
+                $item = Warehouse::find($id);
+                $item->item_name = $request->input('item_name');
+                $item->save();
+
+                LogReport::create(array(
+                    'user_id'=>auth()->user()->id,
+                    'type'=>'edit warehouse item',
+                    'data'=> $item,
+                ));
             }else{
                 $item = Warehouse::create(array(
                     'item_name'=> $request->input('item_name'),
                 ));
 
-                WarehouseStock::create(array(
+                $data = WarehouseStock::create(array(
                     'item_id'=> $item->id,
                     'qty'=> $request->input('qty'),
+                ));
+
+                LogReport::create(array(
+                    'user_id'=>auth()->user()->id,
+                    'type'=>'add warehouse item',
+                    'data'=> $data,
                 ));
                 
             }
@@ -67,7 +80,9 @@ class WarehouseController extends Controller
     }
 
     public function edit(Request $request, $id) {
-        return view('admin.warehouse.edit');
+        $data_bundle = [];
+        $data_bundle['item'] = Warehouse::findOrFail($id);
+        return view('admin.warehouse.edit', compact($data_bundle));
     }
 
     public function addStock(Request $request, $id) {
@@ -86,9 +101,15 @@ class WarehouseController extends Controller
 				'message' => $validation->errors(),
 			], 200);
 		} else {
-            WarehouseStock::create(array(
+            $data = WarehouseStock::create(array(
                 'item_id'=> $item->id,
                 'qty'=> $request->input('qty'),
+            ));
+
+            LogReport::create(array(
+                'user_id'=>auth()->user()->id,
+                'type'=>'add warehouse stock',
+                'data'=> $data,
             ));
 
             return response()->json([
@@ -101,9 +122,18 @@ class WarehouseController extends Controller
 
     public function delete(Request $request, $id){
         // delete
+        Warehouse::where('id', $id)->delete();
+        LogReport::create(array(
+            'user_id'=>auth()->user()->id,
+            'type'=>'delete warehouse item',
+            'data'=> $id,
+        ));
+        return redirect()->back();
     }
 
     public function view(Request $request, $id){
-        return view('admin.warehouse.view');
+        $data_bundle = [];
+        $data_bundle['item'] = Warehouse::findOrFail($id);
+        return view('admin.warehouse.view', compact($data_bundle));
     }
 }
